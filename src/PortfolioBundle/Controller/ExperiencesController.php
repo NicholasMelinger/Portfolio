b8136b51d071554bfee1fba4eb9eb7aadda17cef
NDUP <n.dupont8@gmail.com>
2018-02-25 00:43:52 +0100

    pages experiences et layout de 2nd niveau

--- src/PortfolioBundle/Controller/ExperiencesController.php
+++ src/PortfolioBundle/Controller/ExperiencesController.php
@@ -3,16 +3,35 @@
 namespace PortfolioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use PortfolioBundle\Entity\Experiences;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ExperiencesController extends Controller
 {
     public function experiences_defautAction()
     {
-        return $this->render('PortfolioBundle:Default:experiences.html.twig');
+        return $this->render('PortfolioBundle:Experiences:experiences.html.twig');
     }
-    public function experiences_addAction()
+    public function experiences_addAction(Request $request)
     {
-        return $this->render('PortfolioBundle:Default:experiences.html.twig');
+    	$exp = new Experiences;
+    	$form = $this->createFormBuilder($exp)
+    				 ->add('typeExperience', TextType::class)
+    				 ->add('dureeExperience', TextType::class)
+    				 ->add('descriptionExperience', TextType::class)
+    				 ->add('Valider', SubmitType::class)
+    				 ->getForm();
+    	$form->handleRequest($request);
+    	if ($form->isValid()) {
+    		$em = $this->getDoctrine()->getManager();
+    		$em->persist($exp);
+    		$em->flush();
+    		return $this->redirectToRoute('experience_defaut');
+    	}
+        return $this->render('PortfolioBundle:Experiences:experiences_add.html.twig', array('form_add_exp' => $form->createView()));
     }
 
 }
