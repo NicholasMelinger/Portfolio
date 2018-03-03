a31359f323127abd26cbcfe5713ad752be877fe2
clabault.benjamin <clabault.benjamin@gmail.com>
2018-03-03 19:04:39 +0100

    recherche avancee

--- src/PortfolioBundle/Controller/RechercheController.php
+++ src/PortfolioBundle/Controller/RechercheController.php
@@ -55,17 +55,97 @@ class RechercheController extends Controller
 
         // Exécution de la requête.
         $nomRecherche = $_POST['nomRecherche'];
-        $requeteNomUser = 'SELECT * FROM Utilisateurs 
-        WHERE (nom_utilisateur LIKE "%'.$nomRecherche.'%") 
-        OR (prenom_utilisateur LIKE "%'.$nomRecherche.'%") 
-        OR (CONCAT(nom_utilisateur, " ", prenom_utilisateur) LIKE "%'.$nomRecherche.'%")
-        OR (CONCAT(prenom_utilisateur, " ", nom_utilisateur) LIKE "%'.$nomRecherche.'%")';
-
-
-
+        $requeteNomUser = 'SELECT id, nom_utilisateur, prenom_utilisateur, TIMESTAMPDIFF(YEAR, date_naissance, CURDATE()) AS age
+                            FROM Utilisateurs 
+                            WHERE (nom_utilisateur LIKE "%'.$nomRecherche.'%") 
+                            OR (prenom_utilisateur LIKE "%'.$nomRecherche.'%") 
+                            OR (CONCAT(nom_utilisateur, " ", prenom_utilisateur) LIKE "%'.$nomRecherche.'%")
+                            OR (CONCAT(prenom_utilisateur, " ", nom_utilisateur) LIKE "%'.$nomRecherche.'%")';
         $reponse = $bdd->query($requeteNomUser);
-        //$reponse = $bdd->query('SELECT * FROM Themes');
 
-        return $this->render('PortfolioBundle:Recherche:recherche.html.twig', array('resultatRequete' => $reponse, 'userRecherche' => $nomRecherche));//, array('form' => $form->createView()));
+        // Récupération des associations cursus/users.
+        $requeteCursus = 'SELECT utilisateurs_id, libelle_formation, annee 
+                            FROM cursus_utilisateurs_competences JOIN Cursus ON Cursus.id = cursus_utilisateurs_competences.cursus_id';
+        $resultatCursus = $bdd->query($requeteCursus);
+
+
+        // Récupération des associations compétences/users.
+        $requeteComp = 'SELECT utilisateurs_id, libelle_competence
+                            FROM utilisateurs_competences JOIN Competences ON Competences.id = utilisateurs_competences.competences_id';
+        $resultatComp = $bdd->query($requeteComp);
+
+        
+        
+
+        return $this->render('PortfolioBundle:Recherche:recherche.html.twig', 
+            array('resultatRequete' => $reponse, 'userRecherche' => $nomRecherche, 'resultatCursus' => $resultatCursus->fetchAll(), 'resultatComp' => $resultatComp->fetchAll()));
+
+    }
+
+    public function rechercheAvanceeAction(Request $request)
+    {
+        // Connexion à la BDD.
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=db_portfolio;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+            // En cas d'erreur, on affiche un message et on arrête tout.
+            die('Erreur : '.$e->getMessage());
+        }
+
+        // Récupération des cursus existant en BDD.
+        $requeteCursus = 'SELECT id, libelle_formation FROM Cursus ORDER BY libelle_formation';
+        $resultatCursus = $bdd->query($requeteCursus);
+
+
+
+        // Exécution de la requête.
+        // $nomRecherche = $_POST['nomRecherche'];
+        // $requeteNomUser = 'SELECT id, nom_utilisateur, prenom_utilisateur, TIMESTAMPDIFF(YEAR, date_naissance, CURDATE()) AS age
+        //                     FROM Utilisateurs 
+        //                     WHERE (nom_utilisateur LIKE "%'.$nomRecherche.'%") 
+        //                     OR (prenom_utilisateur LIKE "%'.$nomRecherche.'%") 
+        //                     OR (CONCAT(nom_utilisateur, " ", prenom_utilisateur) LIKE "%'.$nomRecherche.'%")
+        //                     OR (CONCAT(prenom_utilisateur, " ", nom_utilisateur) LIKE "%'.$nomRecherche.'%")';
+        // $reponse = $bdd->query($requeteNomUser);
+
+        // // Récupération des associations cursus/users.
+        // $requeteCursus = 'SELECT utilisateurs_id, libelle_formation, annee 
+        //                     FROM cursus_utilisateurs_competences JOIN Cursus ON Cursus.id = cursus_utilisateurs_competences.cursus_id';
+        // $resultatCursus = $bdd->query($requeteCursus);
+
+
+        // // Récupération des associations compétences/users.
+        // $requeteComp = 'SELECT utilisateurs_id, libelle_competence
+        //                     FROM utilisateurs_competences JOIN Competences ON Competences.id = utilisateurs_competences.competences_id';
+        // $resultatComp = $bdd->query($requeteComp);
+
+        
+        
+
+        return $this->render('PortfolioBundle:Recherche:rechercheavancee.html.twig', array('resultatCursus' => $resultatCursus->fetchAll()));
+
+    }
+
+    public function resultatRechercheAction(Request $request)
+    {
+        // Connexion à la BDD.
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=db_portfolio;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+            // En cas d'erreur, on affiche un message et on arrête tout.
+            die('Erreur : '.$e->getMessage());
+        }
+
+        // Récupération des cursus existant en BDD.
+        $requeteCursus = 'SELECT id, libelle_formation FROM Cursus ORDER BY libelle_formation';
+        $resultatCursus = $bdd->query($requeteCursus);
+
+        return $this->render('PortfolioBundle:Recherche:resultatrecherche.html.twig', array('resultatCursus' => $resultatCursus->fetchAll()));
     }
 }
