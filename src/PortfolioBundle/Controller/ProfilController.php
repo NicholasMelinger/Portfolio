e8bcb14ac4755074a23ab2a87332302d6cb1c4d1
Nicholas.Melinger <nicholas.melinger@gmail.com>
2018-03-07 21:16:55 +0100

    pull nicholas 07/03 21h

--- src/PortfolioBundle/Controller/ProfilController.php
+++ src/PortfolioBundle/Controller/ProfilController.php
@@ -35,7 +35,7 @@ class ProfilController extends Controller
             $passwordEncoder = $this->get('security.password_encoder');
             $password = $passwordEncoder->encodePassword($utilisateur, $utilisateur->getPlainPassword());
             $utilisateur->setPassword($password);
-            $utilisateur->setDateInscription(new \DateTime("11-11-1990"));    
+            $utilisateur->setDateInscription(new \DateTime(date("d-m-y")));    
 
             // 4) save the User!
             $em = $this->getDoctrine()->getManager();
@@ -53,5 +53,35 @@ class ProfilController extends Controller
         ));
 
         //return $this->render('PortfolioBundle:Default:index.html.twig');
+    }
+
+
+    public function modificationProfilAction(Request $request, $id_utilisateur)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $utilisateur = $em->getRepository('PortfolioBundle:Utilisateurs')->find($id_utilisateur);
+
+        $editForm = $this->createForm('PortfolioBundle\Form\UtilisateursType', $utilisateur);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($utilisateur);
+            $em->flush();
+
+
+            return $this->render('PortfolioBundle:Default:index.html.twig');
+            //return $this>redirectToRoute('rayon_edit', array('id' => $rayon->getId()));-
+        }
+
+        /*return $this->render('rayon/edit.html.twig', array(
+            'rayon' => $rayon,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));*/
+
+        return $this->render('PortfolioBundle:Profil:modificationProfil.html.twig',array(
+            'utilisateur' => $utilisateur,
+            'edit_form' => $editForm->createView()));
     }
 }
--- src/PortfolioBundle/Form/UtilisateursType.php
+++ src/PortfolioBundle/Form/UtilisateursType.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UtilisateursType extends AbstractType
@@ -18,25 +20,30 @@ class UtilisateursType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nomUtilisateur')
-                ->add('prenomUtilisateur')
-                ->add('mailUtilisateur', EmailType::class)
-                ->add('username')
+        $builder->add('nomUtilisateur',TextType::class,array('label'=>'Nom : *'))
+                ->add('prenomUtilisateur',TextType::class,array('label'=>'Prénom : *'))
+                ->add('mailUtilisateur', EmailType::class,array('label'=>'Adresse email : *'))
+                ->add('username',TextType::class,array('label'=>'Nom dutilisateur (celui-ci vous servira lors de votre connexion, au même titre que votre adresse email) : *'))
                 ->add('plainPassword', RepeatedType::class, array(
                     'type' => PasswordType::class,
-                    'first_options'  => array('label' => 'Mot de passe'),
-                    'second_options' => array('label' => 'Confirmer le mot de passe'),
+                    'first_options'  => array('label' => 'Mot de passe : *'),
+                    'second_options' => array('label' => 'Confirmer le mot de passe : *'),
                     ))
-                ->add('numeroMobile')
-                ->add('numeroFixe')
-                ->add('adressePostale')
-                ->add('ville')
-                ->add('codePostal')
-                ->add('dateNaissance')
+                ->add('numeroMobile',TextType::class,array('label'=>'Numéro de mobile : '))
+                ->add('numeroFixe', TextType::class,array('label'=>'Numéro de fixe : '))
+                ->add('adressePostale', TextType::class,array('label'=>'Numéro de nom de rue : '))
+                ->add('ville', TextType::class,array('label'=>'Ville : '))
+                ->add('codePostal', TextType::class,array('label'=>'Code postal : '))
+                //->add('dateNaissance', DateType::class, array('label'=>'Date de naissance : '))
+                ->add('dateNaissance',BirthdayType::class, array(
+                        'label'=>'Date de naissance : ',
+                        'format' => 'dd-MM-yyyy') 
+                    )
                 //->add('type_utilisateur');
                 ->add('type_utilisateur', EntityType::class, array(
                     'class'        => 'PortfolioBundle:Types_utilisateur',
-                    'choice_label' => 'libelle'
+                    'choice_label' => 'libelle',
+                    'label'=>'Vous êtes : * '
                   ));
     }/**
      * {@inheritdoc}
