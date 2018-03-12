7fed520af428d87c5058a9e2d88ae69e34df5849
Nicholas.Melinger <nicholas.melinger@gmail.com>
2018-03-12 17:29:29 +0100

    commit

--- src/PortfolioBundle/Controller/ProfilController.php
+++ src/PortfolioBundle/Controller/ProfilController.php
@@ -65,8 +65,61 @@ class ProfilController extends Controller
 
     public function modificationProfilAction($id, Request $request)
     {
+        // Connexion à la BDD.
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=db_portfolio;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+            // En cas d'erreur, on affiche un message et on arrête tout.
+            die('Erreur : '.$e->getMessage());
+        }
+
+
+
         $em = $this->getDoctrine()->getManager();
         $exp = $em->getRepository('PortfolioBundle:Utilisateurs')->find($id);
+
+
+        $requeteCompetence = 'SELECT utilisateurs.id AS id_utilisateur, competences.id AS id_competence, competences.libelle_competence, utilisateurs_competences.niveau_competence, utilisateurs_competences.detail_competence, themes.libelle_theme, sous_themes.libelle_sous_theme, sous_sous_themes.libelle_sous_sous_theme, cursus.libelle_formation 
+            FROM utilisateurs 
+            LEFT JOIN utilisateurs_competences ON utilisateurs_competences.utilisateurs_id = utilisateurs.id 
+            LEFT JOIN competences ON competences.id = utilisateurs_competences.competences_id 
+            LEFT JOIN cursus_utilisateurs_competences ON cursus_utilisateurs_competences.competences_id = competences.id 
+            LEFT JOIN cursus ON cursus.id = cursus_utilisateurs_competences.cursus_id
+            LEFT JOIN matrice ON matrice.id = competences.matrice_comp_id
+            LEFT JOIN themes ON themes.id = matrice.theme_matrice_id
+            LEFT JOIN sous_themes ON sous_themes.id = matrice.s_theme_matrice_id
+            LEFT JOIN sous_sous_themes ON sous_sous_themes.id = matrice.s_s_theme_matrice_id
+            WHERE utilisateurs.id = '. $id .' AND cursus_utilisateurs_competences.utilisateurs_id = ' . $id;
+
+
+        $competences = $bdd->query($requeteCompetence);
+
+
+
+        //Récupérer ses cursus
+        $requeteCursus = 'SELECT cursus.libelle_formation, cursus.description_formation, 
+                            CASE cursus_utilisateurs_competences.diplome WHEN 0 THEN \'Non\' ELSE \'Oui\' END AS diplome,
+                            cursus_utilisateurs_competences.annee
+                            FROM cursus, cursus_utilisateurs_competences 
+                            WHERE cursus_utilisateurs_competences.cursus_id = cursus.id
+                            AND utilisateurs_id = ' . $id;
+        $cursus = $bdd->query($requeteCursus);
+
+
+        //Récupérer ses experiences 
+        $requeteExperience = 'SELECT experiences.type_experience, experiences.description_experience, experiences.dureeExperience, 
+                                CASE cursus.libelle_formation WHEN NULL THEN \'\' ELSE CONCAT( \'Expérience effectués au cours du cursus \' ,cursus.libelle_formation )  END AS cursus_libelle
+                                FROM experiences, utilisateurs, cursus
+                                WHERE experiences.utilisateurs_id = utilisateurs.id
+                                AND cursus.id = experiences.cursus_id
+                                AND utilisateurs.id = ' . $id;
+        $experiences = $bdd->query($requeteExperience);
+
+
+
         if (null === $exp) {
           throw new NotFoundHttpException("L'utilisateur ".$id." n'existe pas.");
         }
@@ -80,7 +133,7 @@ class ProfilController extends Controller
 
         
 
-        return $this->render('PortfolioBundle:Profil:modificationProfil.html.twig', array('edit_form' => $form->createView(),'utilisateur' => $exp));
+        return $this->render('PortfolioBundle:Profil:modificationProfil.html.twig', array('edit_form' => $form->createView(),'utilisateur' => $exp, 'competences' => $competences, 'cursus' => $cursus, 'experiences' => $experiences));
     }
 
     public function profilAction($id)
