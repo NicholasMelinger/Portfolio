c78574871f5e7af4d3b57bc649d7b66ee264b23e
Nicholas.Melinger <nicholas.melinger@gmail.com>
2018-03-12 10:30:58 +0100

    commit nicholas 12/03

--- src/PortfolioBundle/Controller/ProfilController.php
+++ src/PortfolioBundle/Controller/ProfilController.php
@@ -11,12 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use \PDO;
 
 class ProfilController extends Controller
 {
-    public function indexAction()
+    public function profilIndexAction()
     {
-        return $this->render('PortfolioBundle:Default:index.html.twig');
+        return $this->render('PortfolioBundle:Profil:test.html.twig');
     }
 
     /**
@@ -38,6 +39,8 @@ class ProfilController extends Controller
             $password = $passwordEncoder->encodePassword($utilisateur, $utilisateur->getPlainPassword());
             $utilisateur->setPassword($password);
             $utilisateur->setDateInscription(new \DateTime(date("d-m-y")));    
+
+            $utilisateur->setUrlPhoto('../../img/default.jpg');
 
             // 4) save the User!
             $em = $this->getDoctrine()->getManager();
@@ -77,6 +80,69 @@ class ProfilController extends Controller
 
     public function profilAction($id)
     {
-        return $this->render('PortfolioBundle:Profil:profil.html.twig');
+        // Connexion à la BDD.
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=db_portfolio;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+            // En cas d'erreur, on affiche un message et on arrête tout.
+            die('Erreur : '.$e->getMessage());
+        }
+
+
+        //Récupérer l'utilisateur
+        $repository = $this
+              ->getDoctrine()
+              ->getManager()
+              ->getRepository('PortfolioBundle:Utilisateurs')
+            ;
+
+        $utilisateur = $repository->find($id);
+
+
+        //Récupérer ses compétences
+        //$competences = $utilisateur->getUserCompetences();
+
+        $requeteCompetence = 'SELECT utilisateurs.id AS id_utilisateur, competences.id AS id_competence, competences.libelle_competence, utilisateurs_competences.niveau_competence, utilisateurs_competences.detail_competence, themes.libelle_theme, sous_themes.libelle_sous_theme, sous_sous_themes.libelle_sous_sous_theme, cursus.libelle_formation 
+            FROM utilisateurs 
+            LEFT JOIN utilisateurs_competences ON utilisateurs_competences.utilisateurs_id = utilisateurs.id 
+            LEFT JOIN competences ON competences.id = utilisateurs_competences.competences_id 
+            LEFT JOIN cursus_utilisateurs_competences ON cursus_utilisateurs_competences.competences_id = competences.id 
+            LEFT JOIN cursus ON cursus.id = cursus_utilisateurs_competences.cursus_id
+            LEFT JOIN matrice ON matrice.id = competences.matrice_comp_id
+            LEFT JOIN themes ON themes.id = matrice.theme_matrice_id
+            LEFT JOIN sous_themes ON sous_themes.id = matrice.s_theme_matrice_id
+            LEFT JOIN sous_sous_themes ON sous_sous_themes.id = matrice.s_s_theme_matrice_id
+            WHERE utilisateurs.id = '. $id .' AND cursus_utilisateurs_competences.utilisateurs_id = ' . $id;
+
+
+        $competences = $bdd->query($requeteCompetence);
+
+
+
+        //Récupérer ses cursus
+        $requeteCursus = 'SELECT cursus.libelle_formation, cursus.description_formation, 
+                            CASE cursus_utilisateurs_competences.diplome WHEN 0 THEN \'Non\' ELSE \'Oui\' END AS diplome,
+                            cursus_utilisateurs_competences.annee
+                            FROM cursus, cursus_utilisateurs_competences 
+                            WHERE cursus_utilisateurs_competences.cursus_id = cursus.id
+                            AND utilisateurs_id = ' . $id;
+        $cursus = $bdd->query($requeteCursus);
+
+
+        //Récupérer ses experiences 
+        $requeteExperience = 'SELECT experiences.type_experience, experiences.description_experience, experiences.dureeExperience, 
+                                CASE cursus.libelle_formation WHEN NULL THEN \'\' ELSE CONCAT( \'Expérience effectués au cours du cursus \' ,cursus.libelle_formation )  END AS cursus_libelle
+                                FROM experiences, utilisateurs, cursus
+                                WHERE experiences.utilisateurs_id = utilisateurs.id
+                                AND cursus.id = experiences.cursus_id
+                                AND utilisateurs.id = ' . $id;
+        $experiences = $bdd->query($requeteExperience);
+
+
+
+        return $this->render('PortfolioBundle:Profil:profil.html.twig', array('utilisateur' => $utilisateur, 'competences' => $competences, 'cursus' => $cursus , 'experiences' => $experiences));
     }
 }
--- src/PortfolioBundle/Repository/UtilisateursRepository.php
+++ src/PortfolioBundle/Repository/UtilisateursRepository.php
@@ -10,4 +10,29 @@ namespace PortfolioBundle\Repository;
  */
 class UtilisateursRepository extends \Doctrine\ORM\EntityRepository
 {
+	public function myFindAll()
+	{
+		// Méthode 1 : en passant par l'EntityManager
+		$queryBuilder = $this->_em->createQueryBuilder()
+		  ->select('a')
+		  ->from($this->_entityName, 'a')
+		;
+		// Dans un repository, $this->_entityName est le namespace de l'entité gérée
+		// Ici, il vaut donc OC\PlatformBundle\Entity\Advert
+
+		// Méthode 2 : en passant par le raccourci (je recommande)
+		$queryBuilder = $this->createQueryBuilder('a');
+
+		// On n'ajoute pas de critère ou tri particulier, la construction
+		// de notre requête est finie
+
+		// On récupère la Query à partir du QueryBuilder
+		$query = $queryBuilder->getQuery();
+
+		// On récupère les résultats à partir de la Query
+		$results = $query->getResult();
+
+		// On retourne ces résultats
+		return $results;
+	}
 }
